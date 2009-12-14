f832193a4e0b612f1047e8476f0aad176d2f2bcd
sergey-miryanov <sergey.miryanov@gmail.com>
2009-12-14 13:52:35 +0500

    Edit: Wrappers for pvt classes were added

--- bs_pvt/src/py_pvt.cpp
+++ bs_pvt/src/py_pvt.cpp
@@ -1,9 +1,11 @@
 /**
- * \file py_pvt.cpp
- * \brief python wrappers
- * \author Miryanov Sergey
- * \date 08.05.2008
- */
+ *       \file  py_pvt.cpp
+ *      \brief  python wrappers for pvt_
+ *     \author  Sergey Miryanov (sergey-miryanov), sergey.miryanov@gmail.com
+ *       \date  08.05.2008
+ *  \copyright  This source code is released under the terms of 
+ *              the BSD License. See LICENSE for more details.
+ * */
 #include "bs_pvt_stdafx.h"
 
 #include "py_pvt.h"
@@ -16,69 +18,31 @@
 #include "pvt_oil.h"
 #include "pvt_water.h"
 
-namespace blue_sky
+#include "export_python_wrapper.h"
+
+namespace blue_sky  {
+namespace python    {
+
+  PY_EXPORTER (pvt_exporter, default_exporter)
+    .add_property ("p_step",          &T::get_p_step)
+    .add_property ("surface_density", &T::get_surface_density)
+    .def ("get_gor_for_pressure",     &T::get_gor_for_pressure)
+    .def ("print",                    &T::print)
+  PY_EXPORTER_END;
+
+  void
+  py_export_pvt ()
   {
-  namespace python
-    {
+    using namespace boost::python;
 
-    template <class pvt_t> void
-    py_export (const char *name)
-    {
-      using namespace boost::python;
+    strategy_exporter::export_base <pvt_base, pvt_exporter> ("pvt");
+    strategy_exporter::export_class <pvt_oil, pvt_base, pvt_exporter> ("pvt_oil");
+    strategy_exporter::export_class <pvt_gas, pvt_base, pvt_exporter> ("pvt_gas");
+    strategy_exporter::export_class <pvt_water, pvt_base, pvt_exporter> ("pvt_water");
+    strategy_exporter::export_class <pvt_dead_oil, pvt_base, pvt_exporter> ("pvt_dead_oil");
+  }
 
-      class_<pvt_t, bases <typename pvt_t::base_t> > (name)
-      .def ("build", &pvt_t::build)
-      .def ("get_p_step", &pvt_t::get_p_step)
-      .def ("get_surface_density", &pvt_t::get_surface_density)
-      .def ("set_surface_density", &pvt_t::set_surface_density)
-      ;
-    }
-
-    template <typename pvt_t>
-    void
-    py_pvt_export (const char * name)
-    {
-      using namespace boost::python;
-
-      class_ <pvt_t, bases <typename pvt_t::base_t> > (name, init <const sp_obj &> ())
-        .def ("calc", &pvt_t::calc)
-        ;
-    }
-
-#define PY_EXPORT_PVT_WATER \
-    py_export <py_pvt_wrapper <pvt_water <base_strategy_fi> > > ("pvt_water_fi_base");  \
-    py_export <py_pvt_wrapper <pvt_water <base_strategy_di> > > ("pvt_water_di_base");  \
-    py_pvt_export <py_pvt_water <pvt_water <base_strategy_fi> > > ("pvt_water_fi");   \
-    py_pvt_export <py_pvt_water <pvt_water <base_strategy_di> > > ("pvt_water_di");
-
-#define PY_EXPORT_PVT_GAS \
-    py_export <py_pvt_wrapper <pvt_gas <base_strategy_fi> > > ("pvt_gas_fi_base");      \
-    py_export <py_pvt_wrapper <pvt_gas <base_strategy_di> > > ("pvt_gas_di_base");      \
-    py_pvt_export <py_pvt_gas <pvt_gas <base_strategy_fi> > > ("pvt_gas_fi");           \
-    py_pvt_export <py_pvt_gas <pvt_gas <base_strategy_di> > > ("pvt_gas_di");
-
-#define PY_EXPORT_PVT_OIL \
-    py_export <py_pvt_wrapper <pvt_oil <base_strategy_fi> > > ("pvt_oil_fi_base");      \
-    py_export <py_pvt_wrapper <pvt_oil <base_strategy_di> > > ("pvt_oil_di_base");      \
-    py_pvt_export <py_pvt_oil <pvt_oil <base_strategy_fi> > > ("pvt_oil_fi");           \
-    py_pvt_export <py_pvt_oil <pvt_oil <base_strategy_di> > > ("pvt_oil_di");
-
-    void
-    py_export_pvt ()
-    {
-      using namespace boost::python;
-
-      class_ <py_pvt_base, bases <py_objbase> > ("pvt_base", init <sp_obj> ());
-
-      PY_EXPORT_PVT_WATER;
-      PY_EXPORT_PVT_GAS;
-      PY_EXPORT_PVT_OIL;
-
-      py_export<py_pvt_wrapper< pvt_dead_oil<base_strategy_fi> > >  ("pvt_dead_oil_fi");
-      py_export<py_pvt_wrapper< pvt_dead_oil<base_strategy_di> > >  ("pvt_dead_oil_di");
-    }
-
-  } // namespace python
+} // namespace python
 } // namespace blue_sky
 
 #endif  // #ifdef BSPY_EXPORTING_PLUGIN
