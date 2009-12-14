a84cb3cd9a31ff2139dbe0097ef0227d4efe388c
sergey-miryanov <sergey.miryanov@gmail.com>
2009-12-14 11:58:54 +0500

    Edit: Add wrappers for scal_3p

--- bs_scal/src/py_scal_wrapper.cpp
+++ bs_scal/src/py_scal_wrapper.cpp
@@ -9,6 +9,8 @@
 #include "py_scal_wrapper.h"
 #include "scal_save_data.h"
 
+#include "export_python_wrapper.h"
+
 namespace blue_sky
   {
   namespace python
@@ -85,117 +87,56 @@ namespace blue_sky
     }
 #endif
 
-    template <typename strategy_t>
-    void py_export_scale_array_holder (const char *name)
-    {
-      using namespace boost::python;
+    PY_EXPORTER (scale_array_holder_exporter, default_exporter)
+    PY_EXPORTER_END;
 
-      class_ < py_scale_array_holder<strategy_t> > (name)
-      .def ("insert_socr", &py_scale_array_holder<strategy_t>::insert_socr)
-      .def ("insert_scr", &py_scale_array_holder<strategy_t>::insert_scr)
-      .def ("insert_sl", &py_scale_array_holder<strategy_t>::insert_sl)
-      .def ("insert_su", &py_scale_array_holder<strategy_t>::insert_su)
-      ;
-    }
+    PY_EXPORTER (scal_data_holder_exporter, default_exporter)
+    PY_EXPORTER_END;
 
-    template <typename strategy_t>
-    void py_export_scal_data_holder (const char *name)
-    {
-      using namespace boost::python;
+    PY_EXPORTER (jfunction_exporter, default_exporter)
+      .add_property ("st_phase",  &T::st_phase)
+      .add_property ("alpha",     &T::alpha)
+      .add_property ("beta",      &T::beta)
+      .add_property ("valid",     &T::valid)
+    PY_EXPORTER_END;
 
-#ifdef _DEBUG
-      class_ < py_scal_data_holder<strategy_t> > (name)
-      .def ("save_raw_data", &py_scal_data_holder<strategy_t>::save_raw_data)
-      .def ("save_data", &py_scal_data_holder<strategy_t>::save_data)
-#endif
-      ;
-    }
-
-    template <typename strategy_t>
-    void py_export_scal_3p (const char *name)
-    {
-      using namespace boost::python;
-
-      class_ < py_scal_3p<strategy_t> > (name)
-      .def ("get_relative_perm",  &py_scal_3p<strategy_t>::get_relative_perm)
-      .def ("get_capillary",      &py_scal_3p<strategy_t>::get_capillary)
-      .def ("get_water_scale",    &py_scal_3p<strategy_t>::get_water_scale)
-      .def ("get_gas_scale",      &py_scal_3p<strategy_t>::get_gas_scale)
-      .def ("get_water_data",     &py_scal_3p<strategy_t>::get_water_data)
-      .def ("get_gas_data",       &py_scal_3p<strategy_t>::get_gas_data)
-      //.def ("set_water_jfunction", &py_scal_3p::set_water_jfunction)
-      //.def ("set_gas_jfunction", &py_scal_3p::set_gas_jfunction)
-      //.def ("set_n_phases", &py_scal_3p::set_n_phases)
-      //.def ("set_phases", &py_scal_3p::set_phases)
-      //.def ("set_rpo_model", &py_scal_3p::set_rpo_model)
-      //.def ("process_model", &py_scal_3p<strategy_t>::process_model)
-      //.def ("set_test_model", &py_scal_3p<strategy_t>::set_test_model)
-      //.def ("set_phase_d", &py_scal_3p::set_phase_d)
-      //.def ("set_sat_d", &py_scal_3p::set_sat_d)
-      ;
-
-    }
-
-    //template <typename strategy_t>
-    //void py_export_test_model (const char *name)
-    //{
-    //  using namespace boost::python;
-
-    //  class_ < py_test_model<strategy_t> > (name)
-    //  .def ("init", &py_test_model<strategy_t>::init)
-    //  ;
-    //}
-
-    template <typename strategy_t>
-    void py_export_jfunction (const char *name)
-    {
-      using namespace boost::python;
-
-      class_ <py_jfunction <strategy_t>, bases <py_objbase> > (name)
-      .add_property ("st_phase", &py_jfunction<strategy_t>::get_st_phase, &py_jfunction<strategy_t>::set_st_phase)
-      .add_property ("alpha", &py_jfunction<strategy_t>::get_alpha, &py_jfunction<strategy_t>::set_alpha)
-      .add_property ("beta", &py_jfunction<strategy_t>::get_beta, &py_jfunction<strategy_t>::set_beta)
-      .add_property ("valid", &py_jfunction<strategy_t>::valid)
-      .def ("init", &py_jfunction<strategy_t>::init)
-      ;
-    }
+    PY_EXPORTER (scal_3p_exporter, default_exporter)
+      .add_property ("water_scale",      &T::get_water_scale)
+      .add_property ("gas_scale",        &T::get_gas_scale)
+      .add_property ("water_data",       &T::get_water_data)
+      .add_property ("gas_data",         &T::get_gas_data)
+      .add_property ("water_jfunction",  &T::get_water_jfunction, &T::set_water_jfunction)
+      .add_property ("gas_jfunction",    &T::get_gas_jfunction, &T::set_gas_jfunction)
+    PY_EXPORTER_END;
 
     void py_export_scal ()
     {
       using namespace boost::python;
 
       enum_ <PHASE_ENUM> ("phase_enum")
-      .value ("water", PHASE_WATER)
-      .value ("gas", PHASE_GAS)
-      .value ("oil", PHASE_OIL)
-      ;
+        .value ("water", PHASE_WATER)
+        .value ("gas", PHASE_GAS)
+        .value ("oil", PHASE_OIL)
+        ;
 
       enum_ <RPO_MODEL_ENUM> ("rpo_model_enum")
-      .value ("default_model", RPO_DEFAULT_MODEL)
-      .value ("stone1_model", STONE1_MODEL)
-      .value ("stone2_model", STONE2_MODEL)
-      ;
+        .value ("default_model", RPO_DEFAULT_MODEL)
+        .value ("stone1_model", STONE1_MODEL)
+        .value ("stone2_model", STONE2_MODEL)
+        ;
 
       enum_ <JFUNC_PERM_TYPE_ENUM> ("jfunc_perm_type_enum")
-      .value ("xy", JFUNC_PERM_XY)
-      .value ("x", JFUNC_PERM_X)
-      .value ("y", JFUNC_PERM_Y)
-      .value ("z", JFUNC_PERM_Z)
-      ;
+        .value ("xy", JFUNC_PERM_XY)
+        .value ("x", JFUNC_PERM_X)
+        .value ("y", JFUNC_PERM_Y)
+        .value ("z", JFUNC_PERM_Z)
+        ;
 
-      py_export_jfunction <base_strategy_fi> ("jfunction_fi");
-      py_export_jfunction <base_strategy_di> ("jfunction_di");
-
-      py_export_scale_array_holder <base_strategy_fi> ("scale_arrays_fi");
-      py_export_scale_array_holder <base_strategy_di> ("scale_arrays_di");
-
-      py_export_scal_data_holder <base_strategy_fi> ("scal_data_fi");
-      py_export_scal_data_holder <base_strategy_di> ("scal_data_di");
-
-      py_export_scal_3p <base_strategy_fi> ("scal_3p_fi");
-      py_export_scal_3p <base_strategy_di> ("scal_3p_di");
+      strategy_exporter::export_base <jfunction, jfunction_exporter> ("jfunction");
+      strategy_exporter::export_base <scale_array_holder, scale_array_holder_exporter> ("scale_arrays");
+      strategy_exporter::export_base <scal_2p_data_holder, scal_data_holder_exporter> ("scal_data");
+      strategy_exporter::export_base <scal_3p, scal_3p_exporter> ("scal_3p");
     }
-
   }
 }
 
