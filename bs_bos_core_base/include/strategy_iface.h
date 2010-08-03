4b155a00720811d8de3dd0b98bfbe5bf07ce1881
Oleg Borschuk <oleg.borschuk@gmail.com>
2010-08-03 09:59:27 +0600

    new emplementation of matrix base classes with iterfaces

---
+++ bs_bos_core_base/include/strategy_iface.h
@@ -0,0 +1,21 @@
+#ifndef __STRATEGY_IFACE_H
+#define __STRATEGY_IFACE_H
+
+
+namespace blue_sky
+{
+
+  /**
+  * \brief strategy interface
+  */
+  class strategy_iface
+    {
+    public:
+      virtual ~strategy_iface ()
+        {}
+
+    public:
+
+    };
+};
+#endif //__STRATEGY_IFACE_H
