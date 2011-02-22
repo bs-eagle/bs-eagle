0f89a645dd497bc37529e3d58b538c46d440262d
Oleg Borschuk <oleg.borschuk@gmail.com>
2011-02-22 15:58:30 +0500

    added conf.h with definition of the base types

---
+++ bs_bos_core_base/include/conf.h
@@ -0,0 +1,21 @@
+/** 
+ * @file conf.h
+ * @brief define default types  
+ * @author Oleg Borschuk
+ * @version 0.1
+ * @date 2011-02-22
+ */
+#ifndef CONF_7FBMZIFT
+
+#define CONF_7FBMZIFT
+
+#define t_int           long
+#define t_long          long
+#define t_float         double
+#define t_double        double
+
+#define t_uint          unsigned long
+#define t_ulong         unsigned long
+
+
+#endif /* end of include guard: CONF_7FBMZIFT */
