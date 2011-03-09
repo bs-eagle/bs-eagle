45de327f078af52c8d5f5f8ebb8814766b93bb05
uentity <unientity@gmail.com>
2011-03-09 13:22:33 +0500

    -- Improove & update sample refine_mesh script

--- examples/refine_mesh_test.py
+++ examples/refine_mesh_test.py
@@ -7,8 +7,8 @@ import bs.bs_mesh as bm
 dx = numpy.array([25], dtype='d') # Lx
 dy = numpy.array([25], dtype='d') # Ly
 dz = numpy.array([1], dtype='d') # Lz
-Nx = 1000
-Ny = 1000
+Nx = 100
+Ny = 100
 Nz = 1
 [c, z] = bm.mesh_grdecl_did.gen_coord_zcorn(Nx, Ny, Nz, dx, dy, dz)
 print 'Source mesh generated!'
@@ -19,15 +19,20 @@ Wx = 0.05
 Wy = 0.05
 ax = 2
 ay = 2
-vx = numpy.random.rand(200) * 25000;
-vy = numpy.random.rand(200) * 25000;
+points_num = 3
+vx = numpy.random.rand(points_num) * (dx[0] * Nx);
+vy = numpy.random.rand(points_num) * (dy[0] * Ny);
 vax = numpy.zeros(vx.size);
 vax.fill(ax);
 vay = numpy.zeros(vy.size);
 vay.fill(ay);
 vWx = numpy.random.rand(vx.size) * 10;
 vWy = numpy.random.rand(vy.size) * 10;
-p = numpy.reshape(numpy.column_stack([vx, vy, vWx, vWy, vax, vay]), [1, -1]);
+p = numpy.column_stack([vx, vy, vWx, vWy, vax, vay]);
+# dump points to file
+numpy.savetxt("points.txt", p, fmt="%8.4f");
+# convert to 1D array
+p = numpy.reshape(p, [1, -1]);
 #p = numpy.array([
 #	1000, 5000, Wx, Wy, ax, ay,
 #	5000, 5000, Wx, Wy, ax, ay,
@@ -35,7 +40,7 @@ p = numpy.reshape(numpy.column_stack([vx, vy, vWx, vWy, vax, vay]), [1, -1]);
 #	], dtype='d')
 
 # generate refined mesh
-[c1, z1, Nx, Ny] = bm.mesh_grdecl_did.refine_mesh(Nx, Ny, c, z, p, 0.8, 0.3)
+[c1, z1, Nx, Ny, hit_idx] = bm.mesh_grdecl_did.refine_mesh(Nx, Ny, c, z, p, 0.8, 0.3)
 print 'refine finisfed!'
 print 'Nx = ', Nx
 print 'Ny = ', Ny
@@ -52,4 +57,7 @@ f.write('/\n\nZCORN\n')
 numpy.savetxt(f, z1.reshape([-1, 2]), fmt="%8.4f");
 f.write('/')
 f.close();
+
+# save indexes to text file
+numpy.savetxt('hit_idx.txt', hit_idx.reshape([-1, 2]), fmt="%G")
 
