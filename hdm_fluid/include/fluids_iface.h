c38dd8205652ed1a603d57135e056640e02e5d37
Oleg Borschuk <oleg.borschuk@gmail.com>
2011-03-30 09:49:22 +0600

    added initial version of hdm_fluid storage

---
+++ hdm_fluid/include/fluids_iface.h
@@ -0,0 +1,113 @@
+/** 
+ * @file fluids_iface.h
+ * @brief Interface for Fluids storage (PVT, SCAL)
+ * @author Oleg Borschuk
+ * @version 1.0
+ * @date 2011-03-29
+ */
+#ifndef FLUIDS_IFACE_YCX3MW5G
+
+#define FLUIDS_IFACE_YCX3MW5G
+
+
+#include "bs_assert.h"
+#include "bs_tree.h"
+#include "smart_ptr.h"
+#include "bs_array.h"
+#include "conf.h"
+
+#include <string>
+
+namespace blue_sky
+{
+  /** 
+   * @brief PVT interface
+   */
+  class fluids_iface: public objbase
+  {
+    public:
+
+      /** 
+       * @brief destructor
+       */
+       virtual ~fluids_iface () {}
+
+       /** 
+        * @brief return number of phases in the model, 
+        *        if class non initialized allready or phases non specified
+        *        return -1
+        */
+       virtual t_long get_n_phases () const = 0;
+
+       /** 
+        * @brief change the number of PVT regions
+        * 
+        * @param n_regs -- <INPUT> new number of PVT regions
+        */
+       virtual void set_n_pvt_regs (const t_long n_regs) = 0;
+
+       /** 
+        * @brief change the number of SCAL regions
+        * 
+        * @param n_regs -- <INPUT> new number of SCAL regions
+        */
+       virtual void set_n_scal_regs (const t_long n_regs) = 0;
+
+       /** 
+        * @brief change the number of EQUIL regions
+        * 
+        * @param n_regs -- <INPUT> new number of EQUIL regions
+        */
+       virtual void set_n_equil_regs (const t_long n_regs) = 0;
+
+       /** 
+        * @brief make checking of internal state and prepare properties for 
+        *        calculation process
+        *        Call this method before visualizing any fluid property
+        * 
+        * @return 0 if success, < 0 if error occur
+        */
+       virtual int update () = 0;
+
+
+       /** 
+        * @brief return number of PVT regions
+        */
+       virtual t_long get_n_pvt_regs () const = 0;
+
+       /** 
+        * @brief return number of SCAL regions
+        */
+       virtual t_long get_n_scal_regs () const = 0;
+
+       /** 
+        * @brief return number of EQUIL regions
+        */
+       virtual t_long get_n_equil_regs () const = 0;
+
+       /** 
+        * @brief set phases 
+        * 
+        * @param water  -- <INPUT> 0 -- with out water, 1 -- with water
+        * @param oil    -- <INPUT> 0 -- with out oil, 1 -- oil with out desolvet gas,
+        *                          2 -- oil with desolved gas
+        * @param gas    -- <INPUT> 0 -- with out gas, 1 -- with dry gas only,
+        *                          2 -- gas and vaporised oil
+        * 
+        * @return 0 if ok, < 0 if error occur
+        */
+       virtual int set_phases (const t_long water, 
+                               const t_long oil, 
+                               const t_long gas) = 0;
+
+
+
+#ifdef BSPY_EXPORTING_PLUGIN
+      virtual std::string py_str () const = 0;
+#endif //BSPY_EXPORTING_PLUGIN
+
+  };
+};
+
+#endif /* end of include guard: FLUIDS_IFACE_YCX3MW5G */
+
---
+++ hdm_fluid/src/main.cpp
@@ -0,0 +1,37 @@
+/**
+ * \file main.cpp
+ * \brief
+ * \author Sergey Miryanov
+ * \date 23.03.2009
+ * */
+
+#include "pvt_dead_oil.h"
+
+using namespace blue_sky;
+using namespace blue_sky::python;
+using namespace boost::python;
+
+namespace blue_sky {
+  BLUE_SKY_PLUGIN_DESCRIPTOR_EXT ("hdm_fluid", "1.0.0", "Fluid representation for hydrodynamic simulator", "Fluid representation for hydrodynamic simulator", "hdm_fluid")
+
+  BLUE_SKY_REGISTER_PLUGIN_FUN
+  {
+    //const plugin_descriptor &pd = *bs_init.pd_;
+
+    bool res = true;
+
+    res &= BS_KERNEL.register_type(*bs_init.pd_, pvt_dead_oil::bs_type()); BS_ASSERT (res);
+
+    return res;
+  }
+}
+//#if 0
+#ifdef BSPY_EXPORTING_PLUGIN
+BLUE_SKY_INIT_PY_FUN
+{
+  using namespace boost::python;
+
+}
+#endif
+
+//#endif //0
