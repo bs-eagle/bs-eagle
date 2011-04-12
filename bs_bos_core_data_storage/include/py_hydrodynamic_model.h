d662f5b0962b10cdf5aa32bd4bc3dac15d34188f
Mark Khait <mark.l.khait@gmail.com>
2011-04-12 11:54:18 +0600

    Added missed files

---
+++ bs_bos_core_data_storage/include/py_hydrodynamic_model.h
@@ -0,0 +1,13 @@
+#ifndef PY_DATA_MANAGER_H
+#define PY_DATA_MANAGER_H
+
+namespace blue_sky {
+namespace python {
+
+  void
+  py_export_hydrodynamic_model ();
+
+}	// namespace python
+}	// namespace blue_sky
+
+#endif // PY_DATA_MANAGER_H
