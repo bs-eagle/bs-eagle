8807c48a912040427e8525d78123cedca41e7ceb
uentity <unientity@gmail.com>
2011-05-12 09:51:05 +0600

    CZT: move all implementation details to .cpp      Add missing coord_zcorn_tools.h

--- bs_mesh/src/coord_zcorn_tools.cpp
+++ bs_mesh/src/coord_zcorn_tools.cpp
@@ -16,6 +16,647 @@
  *----------------------------------------------------------------*/
 namespace blue_sky { namespace coord_zcorn_tools {
 
+// other typedefs
+typedef bs_array< fp_stor_t, bs_vector_shared > fp_storvec_t;
+typedef smart_ptr< fp_storvec_t > spfp_storvec_t;
+typedef v_long int_arr_t;
+typedef spv_long spi_arr_t;
+typedef std::set< fp_t > fp_set;
+
+typedef fp_storvec_t::iterator v_iterator;
+typedef fp_storvec_t::const_iterator cv_iterator;
+typedef fp_set::iterator fps_iterator;
+
+/*-----------------------------------------------------------------
+ * helper iterators
+ *----------------------------------------------------------------*/
+// iterator that jumps over given offset instead of fixed +1
+template< class iterator_t, int step_size = 1 >
+class slice_iterator : public std::iterator<
+						typename std::iterator_traits< iterator_t >::iterator_category,
+						typename std::iterator_traits< iterator_t >::value_type,
+						typename std::iterator_traits< iterator_t >::difference_type,
+						typename std::iterator_traits< iterator_t >::pointer,
+						typename std::iterator_traits< iterator_t >::reference
+						>
+{
+	typedef std::iterator_traits< iterator_t > traits_t;
+
+public:
+	typedef typename traits_t::value_type value_type;
+	typedef typename traits_t::pointer pointer;
+	typedef typename traits_t::reference reference;
+	typedef typename traits_t::difference_type difference_type;
+
+	// set step in compile-time
+	slice_iterator() : p_(), step_(step_size) {}
+	slice_iterator(const iterator_t& i) : p_(i), step_(step_size) {}
+	slice_iterator(const slice_iterator& i) : p_(i.p_), step_(i.step_) {}
+
+	// set step in runtime
+	slice_iterator(difference_type step) : p_(), step_(step) {}
+	slice_iterator(const iterator_t& i, difference_type step) : p_(i), step_(step) {}
+
+	reference operator[](difference_type i) const {
+		return p_[i * step_];
+	}
+
+	void operator+=(difference_type n) {
+		p_ += n * step_;
+	}
+	void operator-=(difference_type n) {
+		p_ -= n * step_;
+	}
+
+	friend slice_iterator operator+(const slice_iterator& lhs, difference_type n) {
+		return slice_iterator(lhs.p_ + (n * lhs.step_), lhs.step_);
+	}
+	friend slice_iterator operator+(difference_type n, const slice_iterator& lhs) {
+		return slice_iterator(lhs.p_ + (n * lhs.step_), lhs.step_);
+	}
+	friend slice_iterator operator-(const slice_iterator& lhs, difference_type n) {
+		return slice_iterator(lhs.p_ - (n * lhs.step_), lhs.step_);
+	}
+
+	slice_iterator& operator++() {
+		p_ += step_;
+		return *this;
+	}
+	slice_iterator& operator++(int) {
+		slice_iterator tmp = *this;
+		p_ += step_;
+		return tmp;
+	}
+
+	slice_iterator& operator--() {
+		p_ -= step_;
+		return *this;
+	}
+	slice_iterator& operator--(int) {
+		slice_iterator tmp = *this;
+		p_ -= step_;
+		return tmp;
+	}
+
+	pointer operator->() const {
+		return p_.operator->();
+	}
+	reference operator*() const {
+		return *p_;
+	}
+
+	slice_iterator& operator=(const slice_iterator& lhs) {
+		p_ = lhs.p_;
+		return *this;
+	}
+
+	friend difference_type operator-(const slice_iterator& lhs, const slice_iterator& rhs) {
+		return (lhs.p_ - rhs.p_) / lhs.step_;
+	}
+
+	bool operator<(const slice_iterator& rhs) {
+		return p_ < rhs.p_;
+	}
+	bool operator>(const slice_iterator& rhs) {
+		return p_ > rhs.p_;
+	}
+	bool operator<=(const slice_iterator& rhs) {
+		return p_ <= rhs.p_;
+	}
+	bool operator>=(const slice_iterator& rhs) {
+		return p_ >= rhs.p_;
+	}
+	bool operator==(const slice_iterator& rhs) {
+		return p_ == rhs.p_;
+	}
+	bool operator!=(const slice_iterator& rhs) {
+		return p_ != rhs.p_;
+	}
+
+	iterator_t& backend() {
+		return p_;
+	}
+	const iterator_t& backend() const {
+		return p_;
+	}
+
+private:
+	iterator_t p_;
+	const difference_type step_;
+};
+
+// iterator that calc sumulative sum when doing *p
+template< class iterator_t >
+class cumsum_iterator : public std::iterator< std::bidirectional_iterator_tag, typename std::iterator_traits< iterator_t >::value_type > {
+	typedef std::iterator_traits< cumsum_iterator > traits_t;
+
+public:
+	typedef typename traits_t::value_type value_type;
+	typedef typename traits_t::pointer pointer;
+	typedef typename traits_t::reference reference;
+	typedef typename traits_t::difference_type difference_type;
+
+	// set step in compile-time
+	cumsum_iterator(value_type offset = 0) : p_(), sum_(offset) {}
+	cumsum_iterator(const iterator_t& i, value_type offset = 0) : p_(i), sum_(offset) {}
+	cumsum_iterator(const cumsum_iterator& i) : p_(i.p_), sum_(i.sum_) {}
+
+	cumsum_iterator& operator++() {
+		sum_ += *p_++;
+		return *this;
+	}
+	cumsum_iterator& operator++(int) {
+		cumsum_iterator tmp = *this;
+		sum_ += *p_++;
+		return tmp;
+	}
+
+	cumsum_iterator& operator--() {
+		sum_ -= *p_--;
+		return *this;
+	}
+	cumsum_iterator& operator--(int) {
+		cumsum_iterator tmp = *this;
+		sum_ -= *p_--;
+		return tmp;
+	}
+
+	pointer operator->() const {
+		return p_.operator->();
+	}
+	value_type operator*() const {
+		return sum_;
+	}
+
+	cumsum_iterator& operator=(const cumsum_iterator& lhs) {
+		p_ = lhs.p_;
+		sum_ = lhs.sum_;
+		return *this;
+	}
+
+	friend difference_type operator-(const cumsum_iterator& lhs, const cumsum_iterator& rhs) {
+		return lhs.p_ - rhs.p_;
+	}
+
+	bool operator<(const cumsum_iterator& rhs) {
+		return p_ < rhs.p_;
+	}
+	bool operator>(const cumsum_iterator& rhs) {
+		return p_ > rhs.p_;
+	}
+	bool operator<=(const cumsum_iterator& rhs) {
+		return p_ <= rhs.p_;
+	}
+	bool operator>=(const cumsum_iterator& rhs) {
+		return p_ >= rhs.p_;
+	}
+	bool operator==(const cumsum_iterator& rhs) {
+		return p_ == rhs.p_;
+	}
+	bool operator!=(const cumsum_iterator& rhs) {
+		return p_ != rhs.p_;
+	}
+
+	iterator_t& backend() {
+		return p_;
+	}
+	const iterator_t& backend() const {
+		return p_;
+	}
+
+private:
+	iterator_t p_;
+	value_type sum_;
+};
+
+/*-----------------------------------------------------------------
+ * helper structure to get dx[i] regardless of whether it given by one number or by array of
+ * numbers
+ *----------------------------------------------------------------*/
+struct dim_subscript {
+	dim_subscript(const fp_storarr_t& dim, fp_t offset = .0)
+		: dim_(dim), offset_(offset), sum_(offset)
+	{
+		if(dim_.size() == 1)
+			ss_fcn_ = &dim_subscript::ss_const_dim;
+		else
+			ss_fcn_ = &dim_subscript::ss_array_dim;
+	}
+
+	fp_stor_t ss_const_dim(int_t idx) {
+		return static_cast< fp_stor_t >(fp_t(dim_[0] * idx));
+	}
+
+	fp_stor_t ss_array_dim(int_t idx) {
+		if(idx > 0) sum_ += dim_[idx - 1];
+		return static_cast< fp_stor_t>(sum_);
+	}
+
+	void reset() { sum_ = offset_; }
+
+	fp_t operator[](int_t idx) {
+		return (this->*ss_fcn_)(idx);
+	}
+
+private:
+	const fp_storarr_t& dim_;
+	fp_stor_t (dim_subscript::*ss_fcn_)(int_t);
+	const fp_t offset_;
+	fp_t sum_;
+};
+
+/*-----------------------------------------------------------------
+ * misc functions to help generating COORD & ZCORN
+ *----------------------------------------------------------------*/
+template< class array_t >
+void resize_zcorn(array_t& zcorn, int_t nx, int_t ny, int_t new_nx, int_t new_ny) {
+	using namespace std;
+	typedef typename array_t::iterator v_iterator;
+	typedef typename array_t::value_type value_t;
+	typedef typename array_t::size_type size_t;
+
+	const int_t nz = (int_t)(zcorn.size() >> 3) / (nx  * ny);
+	//const int_t delta = 4 * (new_nx * new_ny - nx * ny);
+
+	// cache z-values for each plane
+	const int_t plane_sz = nx * ny * 4;
+	vector< value_t > z_cache(nz * 2);
+	slice_iterator< v_iterator > pz(zcorn.begin(), plane_sz);
+	slice_iterator< v_iterator > pz_end(zcorn.begin(), plane_sz);
+	copy(pz, pz + nz *2, z_cache.begin());
+
+	// refill zcorn with cached values & new plane size
+	const int_t new_plane_sz = (new_nx * new_ny * 4);
+	zcorn.resize(new_nx * new_ny * nz * 8);
+	v_iterator p_newz = zcorn.begin();
+	for(typename vector< value_t >::const_iterator p_cache = z_cache.begin(), end = z_cache.end(); p_cache != end; ++p_cache) {
+		fill_n(p_newz, new_plane_sz, *p_cache);
+  p_newz += new_plane_sz;
+	}
+}
+
+template< class ret_array_t, class array_t >
+void coord2deltas(const array_t& src, ret_array_t& res) {
+	typedef typename array_t::const_iterator carr_iterator;
+
+	if(src.size() < 2) return;
+	res.resize(src.size() - 1);
+	typename ret_array_t::iterator p_res = res.begin();
+	carr_iterator a = src.begin(), b = a, end = src.end();
+	for(++b; b != end; ++b)
+		*p_res++ = *b - *a++;
+}
+
+/*-----------------------------------------------------------------
+ * helper structure related to first refine_mesh algorithm
+ *----------------------------------------------------------------*/
+struct proc_ray {
+	template< int_t direction, class = void >
+	struct dir_ray {
+		enum { dir = direction };
+
+		template< class ray_t >
+		static typename ray_t::iterator end(ray_t& ray) {
+			return ray.end();
+		}
+
+		template< class ray_t >
+		static typename ray_t::iterator last(ray_t& ray) {
+			return --ray.end();
+		}
+
+		template< class ray_t >
+		static typename ray_t::iterator closest_bound(ray_t& ray, fp_t v) {
+			return std::upper_bound(ray.begin(), ray.end(), v);
+		}
+
+		static fp_t min(fp_t f, fp_t s) {
+			return std::min(f, s);
+		}
+
+		static fp_t max(fp_t f, fp_t s) {
+			return std::max(f, s);
+		}
+	};
+
+	template< class unused >
+	struct dir_ray< -1, unused > {
+		enum { dir = -1 };
+
+		template< class ray_t >
+		static typename ray_t::iterator end(ray_t& ray) {
+			return --ray.begin();
+		}
+
+		template< class ray_t >
+		static typename ray_t::iterator last(ray_t& ray) {
+			return ray.begin();
+		}
+
+		template< class ray_t >
+		static typename ray_t::iterator closest_bound(ray_t& ray, fp_t v) {
+			typename ray_t::iterator t = std::upper_bound(ray.begin(), ray.end(), v);
+			if(t == ray.begin()) return t;
+			return t--;
+		}
+
+		static fp_t min(fp_t f, fp_t s) {
+			return std::max(f, s);
+		}
+
+		static fp_t max(fp_t f, fp_t s) {
+			return std::min(f, s);
+		}
+	};
+
+	template< class ray_t, class predicate >
+	static fp_t find_cell(ray_t& coord, predicate p) {
+		typedef typename ray_t::iterator ray_iterator;
+		if(coord.size() < 2) return 0;
+		// find max cell size
+		fp_t cell_sz = 0;
+		ray_iterator a = coord.begin(), b = a, end = coord.end();
+		for(++b; b != end; ++b) {
+			cell_sz = p(*b - *a++, cell_sz);
+			//if(tmp > max_cell) max_cell = tmp;
+		}
+		return cell_sz;
+	}
+
+	template< class ray_t >
+	static fp_t find_max_cell(ray_t& coord) {
+		return find_cell(coord, std::max< fp_t >);
+	}
+
+	template< class ray_t >
+	static fp_t find_min_cell(ray_t& coord) {
+		return find_cell(coord, std::min< fp_t >);
+	}
+
+	template< class ray_t, class dir_ray_t >
+	static void go(ray_t& ray, fp_t start_point, fp_stor_t d, fp_stor_t a, dir_ray_t dr) {
+		using namespace std;
+		const int_t dir = static_cast< int_t >(dir_ray_t::dir);
+		// find where new point fall to
+		const fp_t max_sz = find_max_cell(ray) * 0.5;
+		const fp_t max_front = *dr.last(ray);
+		fp_t cell_sz = d;
+		fp_t wave_front = dr.min(start_point + dir * 0.5 * d, max_front);
+
+		// make refined ray
+		// add refinement grid
+		fp_set ref_ray;
+		while(cell_sz <= max_sz && abs(wave_front - max_front) > 0) {
+			ref_ray.insert(wave_front);
+			cell_sz *= a;
+			wave_front = dr.min(wave_front + dir * cell_sz, max_front);
+		}
+
+		// merge refinement with original grid
+		copy(ray.begin(), ray.end(), insert_iterator< fp_set >(ref_ray, ref_ray.begin()));
+		// copy results back
+		ray.clear();
+		copy(ref_ray.begin(), ref_ray.end(), insert_iterator< ray_t >(ray, ray.begin()));
+	}
+
+	template< class ray_t >
+	static int_t kill_tight_cells(ray_t& ray, fp_t min_cell_sz) {
+		typedef typename ray_t::iterator ray_iterator;
+
+		if(ray.size() < 2) return 0;
+		int_t merge_cnt = 0;
+		ray_iterator a = ray.begin(), b = a, end = ray.end();
+		for(++b; b != end; ++b) {
+			if(*b - *a < min_cell_sz) {
+				ray.erase(a++);
+				++merge_cnt;
+			}
+			else ++a;
+		}
+		return merge_cnt;
+	}
+
+	template< class ray_t >
+	static int_t band_filter(ray_t& ray, fp_t smooth_ratio) {
+		typedef typename ray_t::size_type size_t;
+		typedef std::set< size_t, std::greater< size_t > > idx_set;
+		typedef typename idx_set::const_iterator idx_iterator;
+
+		if(ray.size() < 2) return 0;
+
+		// find cells to remove
+		idx_set dying;
+		//std::vector< size_t > dying;
+		//
+		// left to right walk
+		int_t n = 0;
+		for(size_t i = 0; i < ray.size() - 1; ++i) {
+			// dont check dead bands
+			if(dying.find(i) != dying.end()) continue;
+			// main condition
+			if(ray[i] * smooth_ratio > ray[i + 1]) {
+				dying.insert(i + 1);
+				//dying.push_back(i + 1);
+				// check if cell after bound is less than before bound
+				if(i + 2 < ray.size() && ray[i + 2] < ray[i])
+					ray[i + 2] += ray[i + 1];
+				else
+					ray[i] += ray[i + 1];
+				++n;
+			}
+		}
+
+		// right to left walk
+		for(size_t i = ray.size() - 1; i >= 1; --i) {
+			// dont check dead bands
+			if(dying.find(i) != dying.end()) continue;
+			// main condition
+			if(ray[i] * smooth_ratio > ray[i - 1]) {
+				dying.insert(i - 1);
+				//dying.push_back(i + 1);
+				// check if cell after bound is less than before bound
+				if(i - 2 < ray.size() && ray[i - 2] < ray[i])
+					ray[i - 2] += ray[i - 1];
+				else
+					ray[i] += ray[i - 1];
+				++n;
+			}
+		}
+
+		// remove dead cells
+		for(idx_iterator p_id = dying.begin(), end = dying.end(); p_id != end; ++p_id) {
+			ray.erase(ray.begin() + *p_id);
+		}
+		//for(size_t i = 0; i < dying.size(); ++i)
+		//	ray.erase(ray.begin() + i);
+		return n;
+	}
+};
+
+template< class ray_t >
+void refine_mesh_impl(ray_t& coord, fp_stor_t point, fp_stor_t d, fp_stor_t a) {
+	using namespace std;
+	typedef typename fp_storvec_t::iterator v_iterator;
+	typedef typename fp_storvec_t::const_iterator cv_iterator;
+
+	// sanity check
+	if(!coord.size()) return;
+
+	// process coord in both directions
+	proc_ray::go(coord, point, d, a, typename proc_ray::template dir_ray< 1 >());
+	proc_ray::go(coord, point, d, a, typename proc_ray::template dir_ray< -1 >());
+}
+
+/*-----------------------------------------------------------------
+ * new mesh generation algo based on well points
+ *----------------------------------------------------------------*/
+template< class ray_t, class dir_ray_t >
+void refine_point_s(ray_t& ray, fp_t start_point, fp_stor_t d, fp_stor_t a,
+		fp_stor_t max_sz, fp_stor_t field_len, fp_stor_t closest, dir_ray_t dr)
+{
+	using namespace std;
+	const int_t dir = static_cast< int_t >(dir_ray_t::dir);
+	// DEBUG
+	BSOUT << dir << ": center " << start_point << "; closest " << closest << bs_end;
+
+	// try to calc, how many wave fronts can we insert
+	const uint_t N_max = uint_t(floor(std::log(max_sz / d) / std::log(a) + 1));
+	fp_t S = (dir * (closest - start_point) + d) * 0.5;
+	uint_t N = uint_t(floor(std::log(S * (a - 1)/d + 1) / std::log(a)));
+	N = min< uint_t >(N, N_max);
+	// insert one bound anyway
+	//N = max< uint_t>(N, 1);
+	// calc tail to half of distance to nearest bound
+	fp_t tail = S - d * std::pow(a, N);
+	//if(tail < d * 0.5 && N > 0)
+	//	--N;
+
+	// refined ray stored here
+	fp_set ref_ray;
+	// farthest bound
+	const fp_t max_front = dr.max(0, field_len);
+	fp_t cell_sz = d;
+	fp_t wave_front = dr.min(start_point - dir * 0.5 * d, max_front);
+	
+	for(uint_t i = 0; i < N; ++i) {
+		wave_front += dir * cell_sz;
+		if(i == N -1 && tail < d * 0.5)
+			wave_front += dir * tail;
+		wave_front = dr.min(wave_front, max_front);
+		if(abs(wave_front - max_front) < 0.000001)
+			break;
+		ref_ray.insert(wave_front);
+		cell_sz = min(cell_sz * a, max_sz);
+	}
+	//if(tail > 0 && tail < max_sz) {
+	//	wave_front = dr.min(wave_front + dir * tail, max_front);
+	//	if(wave_front != max_front)
+	//		ref_ray.insert(wave_front);
+	//}
+
+	//while(cell_sz < max_sz && abs(wave_front - max_front) > 0.000001) {
+	//	ref_ray.insert(wave_front);
+	//	cell_sz = min(cell_sz * a, max_sz);
+	//	wave_front = dr.min(wave_front + dir * cell_sz, max_front);
+	//	if(N == 0) break;
+	//	else --N;
+	//}
+
+	// merge refinement with original grid
+	copy(ray.begin(), ray.end(), insert_iterator< fp_set >(ref_ray, ref_ray.begin()));
+	// copy results back
+	ray.clear();
+	copy(ref_ray.begin(), ref_ray.end(), insert_iterator< ray_t >(ray, ray.begin()));
+}
+
+template< class delta_t >
+void fill_gaps(delta_t& d, fp_stor_t cell_sz) {
+	using namespace std;
+	typedef typename delta_t::iterator d_iterator;
+	// fill big gaps with cells of given size
+
+	list< fp_stor_t > refined_d;
+	const fp_t m = 1 / cell_sz;
+	for(d_iterator pd = d.begin(), end = d.end(); pd != end; ++pd) {
+		if(*pd <= cell_sz) {
+			refined_d.push_back(*pd);
+			continue;
+		}
+		// we have long gap here
+		// how much cells can we insert?
+		uint_t N = uint_t(floor(*pd * m));
+		fp_t tail = *pd - N * cell_sz;
+		// if tail is big - push it as separate cell
+		if(tail > 0.3 * cell_sz) {
+			refined_d.push_back(tail);
+			tail = 0;
+		}
+		// otherwise spread tail between first & last
+		for(uint_t i = 0; i < N; ++i) {
+			if((i == 0 || i == N - 1) && tail > 0) {
+				refined_d.push_back(cell_sz + tail * 0.5);
+			}
+			else
+				refined_d.push_back(cell_sz);
+		}
+	}
+
+	// copy refined delta back
+	d.clear();
+	copy(refined_d.begin(), refined_d.end(), insert_iterator< delta_t >(d, d.begin()));
+}
+
+//template< class ray_t >
+//void refine_dim_s(ray_t& ray, fp_stor_t min_d, fp_stor_t max_d, fp_stor_t a, fp_stor_t max_front) {
+//	using namespace std;
+//	typedef typename ray_t::const_iterator ray_iterator;
+//
+//	// we can't make cells larger than max_d
+//	const uint_t N_max = uint_t(ceil(log(max_d/min_d) / log(a)));
+//
+//	fp_set ref_ray;
+//	fp_t wave_front = 0, d;
+//	ref_ray.insert(wave_front);
+//	ray_iterator pr = ray.begin(), pr_end = ray.end();
+//	while(wave_front <= max_front) {
+//		// obtain next cut
+//		if(wave_front > *pr) ++pr;
+//		if(pr != pr_end)
+//			d = *pr - wave_front;
+//		else
+//			d = max_front - wave_front;
+//
+//		fp_t c = wave_front + d * 0.5;
+//		if(d < min_d * 0.5) {
+//			wave_front = c + min_d * 0.5;
+//			// both wells inside one cell
+//			ref_ray.insert(c - min_d * 0.5);
+//			ref_ray.insert(wave_front);
+//		}
+//		else if(d <= min_d) {
+//			// bound between wells
+//			ref_ray.insert(c);
+//			wave_front = c;
+//		}
+//		else {
+//			// calc how much bounds can we insert between wells
+//			fp_t S = (d - min_d) * 0.5;
+//			fp_t n = log(S * (a - 1)/a + 1) / log(a);
+//			int_t N = min(uint_t(ceil(n)), N_max);
+//
+//			fp_t bound = min_d * 0.5;
+//			fp_t cell_sz = a;
+//			for(uint_t i = 0; i < N; ++i) {
+//				ref_ray.insert(wave_front + bound);
+//				ref_ray.insert(*pr - bound);
+//				cell_sz *= a;
+//				bound += cell_sz;
+//			}
+//			//wave_front = 
+//		}
+//	}
+//}
+
 spfp_storarr_t gen_coord(int_t nx, int_t ny, spfp_storarr_t dx, spfp_storarr_t dy, fp_t x0, fp_t y0) {
 	using namespace std;
 	typedef fp_storarr_t::value_type value_t;
