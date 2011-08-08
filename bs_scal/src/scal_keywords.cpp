203427196e27556417aaf328576615d5431a5a62
Mark Khait <mark.l.khait@gmail.com>
2011-08-08 10:56:03 +0600

    added KRO and KRW keywords

--- bs_scal/src/scal_keywords.cpp
+++ bs_scal/src/scal_keywords.cpp
@@ -150,6 +150,8 @@ namespace blue_sky
     keyword_manager->register_fp_pool_keyword ("SWCR",  dimens, 0, 0);
     keyword_manager->register_fp_pool_keyword ("PCW",   dimens, 0, 0);
     keyword_manager->register_fp_pool_keyword ("PCG",   dimens, 0, 0);
+    keyword_manager->register_fp_pool_keyword ("KRO",   dimens, 0, 0);
+    keyword_manager->register_fp_pool_keyword ("KRW",   dimens, 0, 0);
   }
 
   BLUE_SKY_TYPE_STD_CREATE (scal_keywords);
