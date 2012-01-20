f539470c6f0ae6907fa8a14d65b436b5fc84f894
uentity <unientity@gmail.com>
2012-01-20 10:01:48 +0600

    SRZ: implement serialization for locale_keeper

---
+++ bs_bos_core_base/src/locale_keeper_serialize.cpp
@@ -0,0 +1,37 @@
+/// @file locale_keeper_serialize.cpp
+/// @brief Serialization implementation for locale_keeper
+/// @author uentity
+/// @version 
+/// @date 20.01.2012
+/// @copyright This source code is released under the terms of
+///            the BSD License. See LICENSE for more details.
+
+#include "locale_keeper_serialize.h"
+
+#include <boost/archive/polymorphic_iarchive.hpp>
+#include <boost/archive/polymorphic_oarchive.hpp>
+#include <boost/serialization/string.hpp>
+#include <string.h>
+
+using namespace blue_sky;
+
+BLUE_SKY_CLASS_SRZ_FCN_BEGIN(save_construct_data, locale_keeper)
+	std::string loc(t->locale_);
+	ar << loc;
+	ar << t->category_;
+BLUE_SKY_CLASS_SRZ_FCN_END
+
+BLUE_SKY_CLASS_SRZ_FCN_BEGIN(load_construct_data, locale_keeper)
+	std::string loc(t->locale_);
+	int category;
+	ar >> loc;
+	ar >> category;
+	::new(t) locale_keeper(loc.c_str(), category);
+BLUE_SKY_CLASS_SRZ_FCN_END
+
+// empty serialize
+BLUE_SKY_CLASS_SRZ_FCN_BEGIN(serialize, locale_keeper)
+BLUE_SKY_CLASS_SRZ_FCN_END
+
+BOOST_CLASS_EXPORT_IMPLEMENT(locale_keeper)
+
---
+++ bs_bos_core_base/src/locale_keeper_serialize.h
@@ -0,0 +1,22 @@
+/// @file locale_keeper_serialize.h
+/// @brief Serialization declarations for locale_keeper
+/// @author uentity
+/// @version 
+/// @date 20.01.2012
+/// @copyright This source code is released under the terms of
+///            the BSD License. See LICENSE for more details.
+
+#ifndef LOCALE_KEEPER_SERIALIZE_OYZPNZMF
+#define LOCALE_KEEPER_SERIALIZE_OYZPNZMF
+
+#include "bs_serialize.h"
+#include "locale_keeper.h"
+
+BLUE_SKY_CLASS_SRZ_FCN_DECL(serialize, blue_sky::locale_keeper)
+BLUE_SKY_CLASS_SRZ_FCN_DECL(save_construct_data, blue_sky::locale_keeper)
+BLUE_SKY_CLASS_SRZ_FCN_DECL(load_construct_data, blue_sky::locale_keeper)
+
+BOOST_CLASS_EXPORT_KEY(blue_sky::locale_keeper)
+
+#endif /* end of include guard: LOCALE_KEEPER_SERIALIZE_OYZPNZMF */
+
