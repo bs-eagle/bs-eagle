3db7c13b3e87546c5315002dfd64ef4329c19194
uentity <unientity@gmail.com>
2012-01-26 12:45:10 +0600

    SRZ: implement bs_table & rocktab_table serialization

---
+++ bs_bos_core_data_storage/src/rocktab_table_serialize.cpp
@@ -0,0 +1,62 @@
+/// @file rocktab_table_serialize.cpp
+/// @brief bs_table & rocktab_table serialization implementation
+/// @author uentity
+/// @version 
+/// @date 26.01.2012
+/// @copyright This source code is released under the terms of
+///            the BSD License. See LICENSE for more details.
+
+#include "bs_bos_core_data_storage_stdafx.h"
+
+#include "rocktab_table_serialize.h"
+
+#include <boost/serialization/vector.hpp>
+#include <boost/serialization/string.hpp>
+
+using namespace blue_sky;
+namespace boser = boost::serialization;
+typedef unsigned long ulong;
+
+/*-----------------------------------------------------------------
+ * serialize bs_table
+ *----------------------------------------------------------------*/
+BLUE_SKY_CLASS_SRZ_FCN_BEGIN(save, bs_table)
+	// save rows num and col num
+	ar << t.n_rows;
+	ar << (const ulong&)t.columns.size();
+
+	// save data
+	for(ulong i = 0; i < t.columns.size(); ++i) {
+		ar << t.columns[i].name;
+		ar << t.columns[i].data;
+	}
+BLUE_SKY_CLASS_SRZ_FCN_END
+
+BLUE_SKY_CLASS_SRZ_FCN_BEGIN(load, bs_table)
+	// restore rows num
+	ar >> t.n_rows;
+
+	// load data
+	ulong col_num;
+	ar >> col_num;
+	t.columns.resize(col_num);
+	for(ulong i = 0; i < t.columns.size(); ++i) {
+		ar >> t.columns[i].name;
+		ar >> t.columns[i].data;
+	}
+BLUE_SKY_CLASS_SRZ_FCN_END
+
+BLUE_SKY_CLASS_SERIALIZE_SPLIT(bs_table)
+
+BOOST_CLASS_EXPORT_IMPLEMENT(bs_table)
+
+/*-----------------------------------------------------------------
+ * serialize rocktab_table
+ *----------------------------------------------------------------*/
+BLUE_SKY_CLASS_SRZ_FCN_BEGIN(serialize, rocktab_table)
+	// just invoke base class serialization
+	ar & boser::base_object< bs_table >(t);
+BLUE_SKY_CLASS_SRZ_FCN_END
+
+BOOST_CLASS_EXPORT_IMPLEMENT(rocktab_table)
+
