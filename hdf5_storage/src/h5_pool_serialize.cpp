e04eb14822631929d1fd8024fb16b6e0e624ec86
uentity <unientity@gmail.com>
2012-02-01 16:38:53 +0600

    SRZ: fix openning h5 pool when deserializing

--- hdf5_storage/src/h5_pool_serialize.cpp
+++ hdf5_storage/src/h5_pool_serialize.cpp
@@ -31,8 +31,25 @@ BLUE_SKY_CLASS_SRZ_FCN_BEGIN(load, h5_pool)
 	ar >> do_open;
 	ar >> t.fname;
 	ar >> t.path;
-	if(do_open)
-		t.open_file(t.fname, t.path);
+	if(do_open) {
+		t.file_id = H5Fopen(t.fname.c_str(), H5F_ACC_RDWR, H5P_DEFAULT);
+		if(t.file_id < 0) // try to create file
+			t.open_file(t.fname.c_str(), t.path.c_str());
+		else {
+			// try to open group
+			t.group_id = H5Gopen(t.file_id, t.path.c_str());
+			if(t.group_id < 0) {
+				// try to create group
+				t.group_id = H5Gcreate(t.file_id, t.path.c_str (), -1);
+				if(t.group_id < 0)
+					bs_throw_exception(
+						boost::format ("h5_pool_serialize: Can't create group: %s") % t.path
+					);
+			}
+			// init
+			t.fill_map();
+		}
+	}
 	ar >> boser::make_array(&t.pool_dims, 3);
 	ar >> t.n_pool_dims;
 BLUE_SKY_CLASS_SRZ_FCN_END
