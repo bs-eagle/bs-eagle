935105268d1d9eb1ea0e2f570085f77dd98a8572
uentity <unientity@gmail.com>
2012-02-09 15:21:56 +0600

    SRZ: fix pvt_oil base class serialization (pvt_dead_oil)

--- bs_pvt/src/pvt_3p_serialize.cpp
+++ bs_pvt/src/pvt_3p_serialize.cpp
@@ -61,7 +61,7 @@ BLUE_SKY_TYPE_SERIALIZE_IMPL(pvt_gas)
  *----------------------------------------------------------------*/
 BLUE_SKY_CLASS_SRZ_FCN_BEGIN(serialize, pvt_oil)
 	// just redirect to base class
-	ar & boser::bs_base_object< pvt_base >(t);
+	ar & boser::bs_base_object< pvt_dead_oil >(t);
 BLUE_SKY_CLASS_SRZ_FCN_END
 
 BLUE_SKY_TYPE_SERIALIZE_DECL(pvt_oil)
