2684dc68a48be07e8e980cdf3216f924ca9f4f45
uentity <unientity@gmail.com>
2012-11-13 11:22:19 +0600

    WPI: add GRDECL mesh creation and initialization function to handy_mesh interface

--- bs_mesh/src/i_cant_link_2_mesh.cpp
+++ bs_mesh/src/i_cant_link_2_mesh.cpp
@@ -14,6 +14,9 @@
 #include "bs_mesh_grdecl.h"
 #include "well_path_ident.h"
 
+// profiling
+#include <google/profiler.h>
+
 namespace blue_sky {
 
 namespace {
@@ -53,6 +56,15 @@ public:
 		return blue_sky::where_is_point_2d(nx, ny, coord, zcorn, point);
 	}
 
+	sp_smesh make_mesh_grdecl(t_long nx, t_long ny, spv_float coord, spv_float zcorn) {
+		typedef smart_ptr< bs_mesh_grdecl, true > sp_grd_mesh;
+		// build mesh_grdecl around given mesh
+		sp_grd_mesh grd_src = BS_KERNEL.create_object(bs_mesh_grdecl::bs_type());
+		if(!grd_src) return NULL;
+		grd_src->init_props(nx, ny, coord, zcorn);
+		return grd_src;
+	}
+
 	BLUE_SKY_TYPE_DECL(handy_object)
 };
 
@@ -84,7 +96,9 @@ namespace python {
 
 		spv_long cell_idx = BS_KERNEL.create_object(v_long::bs_type());
 		spv_float points = BS_KERNEL.create_object(v_float::bs_type());
+		ProfilerStart("/home/uentity/my_projects/blue-sky.git/gui/enum_border_facets_vtk.prof");
 		spv_long res = wpi_algo::enum_border_facets_vtk(nx, ny, coord, zcorn, mask, cell_idx, points);
+		ProfilerStop();
 		return make_tuple(res, cell_idx, points);
 	}
 
@@ -94,7 +108,9 @@ namespace python {
 
 		spv_long cell_idx = BS_KERNEL.create_object(v_long::bs_type());
 		spv_float points = BS_KERNEL.create_object(v_float::bs_type());
+		ProfilerStart("/home/uentity/my_projects/blue-sky.git/gui/enum_border_edges_vtk.prof");
 		spv_long res = wpi_algo::enum_border_edges_vtk(nx, ny, coord, zcorn, mask, cell_idx, points);
+		ProfilerStop();
 		return make_tuple(res, cell_idx, points);
 	}
 
