482d50fc7143c03c1bb968bb46bd761592d519cf
uentity <unientity@gmail.com>
2014-02-18 19:20:37 +0600

    WPI: move more code to common 3D strategy part

--- bs_wpi/include/strategy_3d_common.h
+++ bs_wpi/include/strategy_3d_common.h
@@ -86,6 +86,108 @@ struct strategy_3d_common {
 			return W[7];
 		}
 	};
+
+	// common cell properties
+	struct cell_data {
+		enum { n_vertex = 8 };
+		enum { n_facets = 6 };
+		enum { n_facet_vertex = 4 };
+		enum { n_edges = 12 };
+
+		// array to hold facet vertices IDs
+		typedef ulong facet_vid_t[n_facet_vertex];
+
+		/* nodes layout
+			*                             X
+			*                    0+-------+1
+			*                    /|     / |
+			*                  /  |   /   |
+			*               2/-------+3   |
+			*              Y |   4+--|----+5
+			*                |   /Z  |   /
+			*                | /     | /
+			*              6 /-------/7
+		*/
+		/*  facets layout (nodes - plane id)
+		*  0-1-2-3 - 0
+		*  0-1-4-5 - 1
+		*  4-5-6-7 - 2
+		*  2-3-6-7 - 3
+		*  0-2-4-6 - 4
+		*  1-3-5-7 - 5
+		*  inside cell - 6
+		*/
+
+		// obtain facet idx from given axe ID (dimension)
+		// and facet ID (0 - starting from origin, 1 - opposite facet)
+		static ulong facet_id(ulong dim, ulong facet) {
+			switch(dim) {
+				// X
+				case 0 :
+					return 4 + facet;
+				// Y
+				case 1 :
+					return 1 + facet*2;
+				// Z
+				case 2 :
+					return 0 + facet*2;
+			}
+			return ulong(-1);
+		}
+
+		// obtain IDs of given facet vertices
+		static const facet_vid_t& facet_vid(ulong facet) {
+			static const facet_vid_t t[6] = {
+				{0, 1, 3, 2}, {0, 1, 5, 4}, {4, 5, 7, 6},
+				{2, 3, 7, 6}, {0, 2, 6, 4}, {1, 3, 7, 5}
+				//{ulong(-1), ulong(-1), ulong(-1), ulong(-1)}
+			};
+			return t[facet];
+		}
+		static void facet_vid(ulong dim, ulong facet, facet_vid_t& res) {
+			return facet_vid(facet_id(dim, facet, res));
+		}
+	};
+
+	// fast chack if bbox intersect with segment
+	template< class Point >
+	static bool bbox_segment_x(
+		const Point& b_min, const Point& b_max,
+		const Point& s_min, const Point& s_max
+		)
+	{
+		double t_min[D], t_max[D];
+		for(uint i = 0; i < D; ++i) {
+			double dd = 0;
+			if(s_min[i] != s_max[i])
+				dd = 1.0 / (s_min[i] - s_max[i]);
+
+			t_min[i] = (b_max[i] - s_min[i]) * dd;
+			t_max[i] = (b_min[i] - s_min[i]) * dd;
+
+			// fisrt check in 2D
+			if(i == 1) {
+				if(t_min[0] > t_max[1] || t_min[1] > t_max[0])
+					return false;
+				t_min[0] = std::max(t_min[0], t_min[1]);
+				t_max[0] = std::min(t_max[0], t_max[1]);
+			}
+		}
+
+		// last check in 3d
+		if(t_min[0] > t_max[2] || t_min[2] > t_max[0])
+			return false;
+		return true;
+	}
+
+	template< class Bbox, class Segment >
+	static bool bbox_segment_x(const Bbox& b, const Segment& s) {
+		return bbox_segment_x(
+			Point(b.min(0), b.min(1), b.min(2)),
+			Point(b.max(0), b.max(1), b.max(2)),
+			s.min(), s.max()
+		);
+	}
 };
 
 }} // eof blue_sky::wpi
