e1f7ae81ce5c64821e2a47f18e2a293885310fd2
uentity <unientity@gmail.com>
2014-10-01 15:09:45 +0600

    -- remove *stdafx.h, *.stdafx.cpp

--- bs_bos_core_base/src/stdafx.cpp
+++
@@ -1,8 +0,0 @@
-// stdafx.cpp : source file that includes just the standard includes
-// bs_bos_core_base.pch will be the pre-compiled header
-// stdafx.obj will contain the pre-compiled type information
-
-#include "bs_bos_core_base_stdafx.h"
-
-// TODO: reference any additional headers you need in STDAFX.H
-// and not in this file
--- bs_mesh/src/stdafx.cpp
+++
@@ -1,8 +0,0 @@
-// stdafx.cpp : source file that includes just the standard includes
-// bs_mesh.pch will be the pre-compiled header
-// stdafx.obj will contain the pre-compiled type information
-
-#include "bs_mesh_stdafx.h"
-
-// TODO: reference any additional headers you need in STDAFX.H
-// and not in this file
