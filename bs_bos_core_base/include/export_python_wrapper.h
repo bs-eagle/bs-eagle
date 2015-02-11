103f9998585b1458dd5bc9b2aff9788d04b2bc6d
uentity <unientity@gmail.com>
2015-02-11 18:24:15 +0500

    bs_bos_core_base: add implicit convertion from sb_obj -> target obj type in Python

--- bs_bos_core_base/include/export_python_wrapper.h
+++ bs_bos_core_base/include/export_python_wrapper.h
@@ -128,6 +128,7 @@ namespace python {
       {
         using namespace boost::python;
         register_ptr_to_python <smart_ptr <class_t_, true> > ();
+        implicitly_convertible <smart_ptr <objbase, true>, smart_ptr <class_t_, true> > ();
       }
       template <typename class_t_, typename base_t_>
       static void
@@ -136,6 +137,7 @@ namespace python {
         using namespace boost::python;
         register_ptr_to_python <smart_ptr <class_t_, true> > ();
         implicitly_convertible <smart_ptr <class_t_, true>, smart_ptr <base_t_, true> > ();
+        implicitly_convertible <smart_ptr <objbase, true>, smart_ptr <class_t_, true> > ();
       }
     };
   }
